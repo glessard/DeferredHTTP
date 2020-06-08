e744eee2621eae44cf1f252fced442bbfc090b76
Guillaume Lessard <dhtnstff@gmail.com>
2020-06-08 16:00:47 -0600

    improve handling of unknown errors in completion handlers

--- Source/DeferredHTTP/Completions.swift
+++ Source/DeferredHTTP/Completions.swift
@@ -19,10 +19,10 @@ internal func dataCompletion(_ resolver: Resolver<(Data, HTTPURLResponse), URLEr
   return {
     (data: Data?, response: URLResponse?, error: Error?) in
 
-    if error != nil
+    if let error = error
     { // note that response isn't necessarily `nil` here,
       // but does it ever contain any data that's not in the Error?
-      let error = (error as? URLError) ?? URLError(.unknown)
+      let error = (error as? URLError) ?? URLError(.unknown, userInfo: [NSUnderlyingErrorKey: error])
       resolver.resolve(error: error)
       return
     }
@@ -30,7 +30,7 @@ internal func dataCompletion(_ resolver: Resolver<(Data, HTTPURLResponse), URLEr
     if let r = response as? HTTPURLResponse
     { resolver.resolve(value: (data ?? Data(), r)) }
     else // Probably an impossible situation
-    { resolver.resolve(error: URLError(.unknown, userInfo: ["unknown": "invalid state at line \(#line)"])) }
+    { resolver.resolve(error: URLError(.unknown, userInfo: ["URLResponse": response as Any, "Data": data as Any])) }
   }
 }
 
@@ -41,10 +41,10 @@ internal func downloadCompletion(_ resolver: Resolver<(FileHandle, HTTPURLRespon
   return {
     (location: URL?, response: URLResponse?, error: Error?) in
 
-    if error != nil
+    if let error = error
     { // note that response isn't necessarily `nil` here,
       // but does it ever contain any data that's not in the Error?
-      let error = (error as? URLError) ?? URLError(.unknown)
+      let error = (error as? URLError) ?? URLError(.unknown, userInfo: [NSUnderlyingErrorKey: error])
       resolver.resolve(error: error)
       return
     }
@@ -60,6 +60,6 @@ internal func downloadCompletion(_ resolver: Resolver<(FileHandle, HTTPURLRespon
       resolver.resolve(value: (handle, response))
     }
     else // can happen if resume data is corrupted; otherwise probably an impossible situation
-    { resolver.resolve(error: URLError(.unknown, userInfo: ["unknown": "invalid state at line \(#line)"])) }
+    { resolver.resolve(error: URLError(.unknown, userInfo: ["URLResponse": response as Any])) }
   }
 }
